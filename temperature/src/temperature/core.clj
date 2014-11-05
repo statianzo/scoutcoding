c1f7f36e7b20e7f53cb5b3a4a4e5ab6d7ace539d
Jason Staten <jstaten07@gmail.com>
2014-11-05 16:49:39 -0700

    temperature conversion works

--- temperature/src/temperature/core.clj
+++ temperature/src/temperature/core.clj
@@ -1,5 +1,22 @@
 (ns temperature.core
   (:gen-class))
 
+;(defn c-to-f [celsius]
+  ;(+ 32 (/ (* 9 celsius) 5))
+;)
+
+(defn c-to-f [celsius]
+  (->
+    celsius
+    (* 9)
+    (/ 5)
+    (+ 32)
+    ))
+
+
 (defn -main [& args]
-  (println "Hello, World!"))
+  (do
+    (println "Whats the temperature in Celsius?")
+    (let [temp (Float/parseFloat (read-line))
+          converted (c-to-f temp)]
+      (println (str "The temperature is " converted " Farenheit")))))
